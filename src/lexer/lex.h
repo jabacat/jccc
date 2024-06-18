a2cd9ae631265507cd01b69ad31468becdcfee8d
adamhutchings <adam.abahot@gmail.com>
2024-06-17 19:22:41 -0700

    Add basic lexer struct

--- src/lexer/lex.h
+++ src/lexer/lex.h
@@ -4,6 +4,13 @@
 
 #include "token.h"
 
+// This is a lexer structure, which will contain all of the information about
+// the state of a lexer. For now all we need is the file pointer which actually
+// has the data we need, but in the future we will need more.
+typedef struct {
+    FILE * fp;
+} Lexer;
+
 // Takes an open file pointer, and a token as input. It fills the token struct
 // with the next available token from the file.
-int lex(FILE * fp, Token * token);
+int lex(Lexer * l, Token * token);
