7bcbef58e9a837726a91b97f07f385c6ad0509b4
adamhutchings <adam.abahot@gmail.com>
2024-06-19 01:59:39 -0700

    Make messages neater

--- src/driver/main.c
+++ src/driver/main.c
@@ -7,7 +7,7 @@
 #include <util/out.h>
 
 int main(int argc, char **argv) {
-    PRINT_DEFAULT("jccc: JabaCat C Compiler 2024");
+    PRINT_DEFAULT("JabaCat C Compiler 2024");
     PRINT_DEBUG("Compilation not supported yet -- coming soon!");
     return 0;
 }
