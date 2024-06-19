f1d1fc4adcfe947735d563ad4bf4a0d7d44522a1
adamhutchings <adam.abahot@gmail.com>
2024-06-19 01:57:23 -0700

    Add output facilities

--- src/driver/main.c
+++ src/driver/main.c
@@ -1,11 +1,13 @@
 /**
  * Main C source code file for the JabaCat C Compiler.
- * Initial commit: June 17, 2024
  */
 
 #include <stdio.h>
 
+#include <util/out.h>
+
 int main(int argc, char **argv) {
-    printf("%s\n", "Hello, World!");
+    PRINT_DEFAULT("jccc: JabaCat C Compiler 2024");
+    PRINT_DEBUG("Compilation not supported yet -- coming soon!");
     return 0;
 }
