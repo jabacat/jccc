f9597565a6b09757034db9e6958ce8e2c1a38ac7
TheCrystalKeeper <87873788+TheCrystalKeeper@users.noreply.github.com>
2024-06-20 17:13:46 -0700

    Update lex.c

--- src/lexer/lex.c
+++ src/lexer/lex.c
@@ -24,3 +24,35 @@ int unlex(Lexer *l, Token *t) {
     l->unlexed_count++;
     return 0;
 }
+
+int skip_to_token(Lexer *l) {
+    char cur;
+    char prev;
+    if ((cur = fgetc(l->fp)) != EOF) {
+        prev = cur;
+        if (!(cur == ' ' || cur == '/')) {
+            fseek(file, -1, SEEK_CUR);
+            return 0; // token begins immediately
+        }
+    } else {
+        return -1; // file done, no more tokens
+    }
+    // read each character from the file until EOF
+    int in_block = 0;
+    while ((cur = fgetc(l->fp)) != EOF) {
+        if (cur == prev && prev == '/')
+            in_block = 1; // single line
+        else if (cur == '*' && prev == '/')
+            in_block = 2; // block
+        else if ((in_block == 1 && cur == '\n') || (in_block == 2 && cur == '/' && prev == '*'))
+            in_block = 0; // out of comment
+
+        if (!(cur == ' ' || cur == '/') && in_block == 0) {
+            fseek(file, -1, SEEK_CUR);
+            return 0; // token is next
+        }
+        prev = cur;
+    }
+    return -1;
+    // eof was reached.
+}
