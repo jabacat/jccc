a84f1d1cd773851a8dc15aeeea75d18a75250f0c
Jake <jakeroggenbuck2@gmail.com>
2024-06-21 22:31:16 -0700

    Add more tokens
    Keywords (other than typedef) added from memory

--- src/lexer/token.h
+++ src/lexer/token.h
@@ -17,6 +17,73 @@ typedef enum {
     TT_CBRACKET, // ]
     TT_SEMI, // ;
     TT_NO_TOKEN, // Not a token
+	
+	TT_PERIOD, // .
+	TT_COMMA, // ,
+	TT_MINUS, // -
+	TT_PLUS, // +
+	TT_STAR, // *
+	TT_SLASH, // /
+	TT_ASSIGN, // =
+	TT_COLON, // :
+	TT_MOD, // %
+	TT_BAND, // &
+	TT_LAND, // &&
+	TT_BOR, // |
+	TT_LOR, // ||
+	TT_DEC, // -=
+	TT_INC, // +=
+	TT_PLUSPLUS, // ++
+	TT_MINUSMINUS, // --
+	TT_DIVEQ, // /=
+	TT_MULEQ, // *=
+	TT_MODEQ, // %=
+	TT_BANDEQ, // &=
+	TT_BOREQ, // |=
+	TT_LANDEQ, // &&=
+	TT_LOREQ, // ||=
+	TT_GREATER, // >
+	TT_LESS, // <
+	TT_LESSEQ, // <=
+	TT_GREATEREQ, // >=
+	TT_LEFTSHIFT, // <<
+	TT_RIGHTSHIFT, // >>
+	TT_LNOT, // !
+	TT_LTILDE, // ~
+	TT_EQUALS, // ==
+	
+	TT_AUTO, // auto
+	TT_BREAK, // break
+	TT_CHAR, // char
+	TT_CONST, // const
+	TT_CASE, // case
+	TT_CONTINUE, // continue
+	TT_DOUBLE, // double
+	TT_DO, // do
+	TT_DEFAULT, // default
+	TT_ENUM, // enum
+	TT_ELSE, // else
+	TT_EXTERN, // extern
+	TT_FLOAT, // float
+	TT_FOR, // for
+	TT_GOTO, // goto
+	TT_IF, // if
+	TT_INT, // int
+	TT_LONG, // long
+	TT_RETURN, // return
+	TT_REGISTER, // register
+	TT_STATIC, // static
+	TT_SWITCH, // switch
+	TT_SHORT, // short
+	TT_SIGNED, // signed
+	TT_STRUCT, // struct
+	TT_SIZEOF, // sizeof
+	TT_typedef, // typedef
+	TT_UNSIGNED, // unsigned
+	TT_UNION, // union
+	TT_VOID, // void
+	TT_VOLATILE, // volitile
+	TT_WHILE, // while
 } TokenType;
 
 // The maximum possible length of a token.
