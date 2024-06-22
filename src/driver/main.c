203bf11bf4137791525937eb9fbb8fb51edfed94
adamhutchings <adam.abahot@gmail.com>
2024-06-22 09:51:03 -0400

    Add basic lexer test framework

--- src/driver/main.c
+++ src/driver/main.c
@@ -4,10 +4,28 @@
 
 #include <stdio.h>
 
+#include <lexer/lex.h>
 #include <util/out.h>
 
+#define TEST_FILE "tests/lextest.c"
+
 int main(int argc, char **argv) {
-    PRINT_DEFAULT("JabaCat C Compiler 2024");
-    PRINT_DEBUG("Compilation not supported yet -- coming soon!");
+    
+    // Initialization of everything
+    Lexer lexer;
+    FILE * fp = fopen(TEST_FILE, "r");
+    if (!fp) {
+        PRINT_ERROR("File %s not found", TEST_FILE);
+        return 1;
+    }
+    lexer.fp = fp;
+    lexer.unlexed_count = 0;
+
+    Token t;
+    do {
+        lex(&lexer, &t);
+        printf("%s\n", t.contents);
+    } while (t.type != TT_EOF);
+
     return 0;
 }
