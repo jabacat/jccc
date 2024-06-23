5c710e33c8897f036dd7a5b75b2cc658b9aa99ea
Jake <jakeroggenbuck2@gmail.com>
2024-06-22 17:01:33 -0700

    Add two tests

---
+++ tests/identtest.c
@@ -0,0 +1,23 @@
+/* We should get the tokens in order:
+	int
+	main
+	(
+	)
+	{
+	int
+	a
+	=
+	100
+	;
+	return
+	a
+	*
+	5
+	;
+	[end of file]
+*/
+
+int main() {
+	int a = 100;
+	return a * 5;
+}
