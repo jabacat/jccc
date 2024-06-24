0e4184c7e2cf7f57b4bbb48510259b1b85cfaafd
adamhutchings <adam.abahot@gmail.com>
2024-06-24 13:32:56 -0400

    Add element-getting fn

--- src/util/list.c
+++ src/util/list.c
@@ -36,3 +36,29 @@ int destroy_list(List* l) {
     free(l);
     return 0;
 }
+
+void* lget_element(List* l, int index) {
+    int i = index;
+    ListBlock* lb = l->head;
+    if (index < 0) {
+        PRINT_ERROR("internal: list index was negative (%d)", index);
+        return NULL;
+    }
+    // We see if the index is in range of the current block length. If not,
+    // repeat with the next block.
+    while (i >= lb->size) {
+        if (lb) {
+            lb = lb->next;
+            i -= lb->size;
+        } else {
+            PRINT_ERROR("internal: list index %d out of bounds", index);
+            return NULL;
+        }
+    }
+    // Now the index is in range and we are at the list block we wish to access.
+    if (i >= lb->full) {
+        PRINT_ERROR("internal: list index %d out of bounds", index);
+        return NULL;
+    }
+    return lb->array[i];
+}
