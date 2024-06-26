aada4f188ea67c104642450869588b047684e227
adamhutchings <adam.abahot@gmail.com>
2024-06-26 09:59:09 -0400

    Add new test

--- src/testing/main.c
+++ src/testing/main.c
@@ -1,9 +1,11 @@
-#include "lexer/test_lexer.h"
 #include "codegen/x86/test_x86.h"
+#include "lexer/test_lexer.h"
+#include <util/test_list.h>
 
 int main() {
 	test_lexer();
 	test_x86();
+	test_list();
 
 	return 0;
 }
