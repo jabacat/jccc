0d0eea0f227be681ccd235fba045a8048047fb4f
Jake <jakeroggenbuck2@gmail.com>
2024-06-29 11:18:54 -0700

    Add risc-v init

--- src/codegen/RISCV/codegen.c
+++ src/codegen/RISCV/codegen.c
@@ -1,1 +1,25 @@
-// Possibility later
+#include <stdint.h>
+
+#include "codegen.h"
+
+#include <testing/tassert.h> // tassert
+
+struct RGenState {
+    // Each bit corresponds with a registers 0-31 where the LSB is 0
+    uint32_t registers_in_use;
+
+    unsigned int sp_offset;
+} RGEN_STATE;
+
+void r_code_gen_init() {
+    RGEN_STATE.registers_in_use = 0;
+    RGEN_STATE.sp_offset = 0;
+}
+
+char *r_start_main() {
+    static char start[256] = "\
+.global _start\n\
+_start:\n";
+
+    return start;
+}
