efbf8e0b5eb2bc01af132a47697182be4854e4c9
Jake <jakeroggenbuck2@gmail.com>
2024-07-31 11:18:38 -0700

    Add test

--- src/testing/main.c
+++ src/testing/main.c
@@ -1,11 +1,13 @@
 #include "codegen/x86/test_x86.h"
 #include "lexer/test_lexer.h"
 #include <util/test_list.h>
+#include <util/test_util.h>
 
 int main() {
 	test_lexer();
 	test_x86();
 	test_list();
+	test_util();
 
 	return 0;
 }
